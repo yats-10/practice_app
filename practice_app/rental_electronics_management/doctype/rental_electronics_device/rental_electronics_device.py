8e0d0b4fa1a63923ff4aa01dfda912152d55cf8d
yats-10 <letstalk.yatharth@gmail.com>
2024-04-07 01:01:05 +0530

    ..

--- practice_app/rental_electronics_management/doctype/rental_electronics_device/rental_electronics_device.py
+++ practice_app/rental_electronics_management/doctype/rental_electronics_device/rental_electronics_device.py
@@ -6,35 +6,35 @@ from frappe.utils import today
 from frappe.model.mapper import get_mapped_doc
 
 class RentalElectronicsDevice(Document):
+    pass
+    # def validate(self):
+    #     self.new_sales_order()
+    #     frappe.msgprint("New Sales Order Has Been Created")
 
-    def validate(self):
-        self.new_sales_order()
-        frappe.msgprint("New Sales Order Has Been Created")
 
-
-    def new_sales_order(self):
-        # doc=frappe.new_doc('Sales Order')
+    # def new_sales_order(self):
+    #     doc=frappe.new_doc('Sales Order')
         
-        # doc.customer=self.customer
-        # itemDoc=frappe.new_doc('Sales Order Item')
-        # itemDoc.qty=self.total_rented_hours
-        # itemDoc.delivery_date=today()
-        # itemDoc.item_code=self.stock_item
-        # itemDoc.gst_hsn_code="999512"
-        # # doc.append('items',{
-        #     'item_code':self.stock_item,
-        #     'delivery_date':today(),
-        #     'qty':self.total_rented_hours,
-        #     'rate':self.hourly_rent,
-        #     'amount':self.total_rented_hours*self.hourly_rent,
-        #     'gst_hsn_code':"999512"
-        # # })
-        doc=frappe.get_doc({
-            'doctype':'Sales Order',
-            'customer':self.customer
-        })
-        doc.insert(ignore_mandatory=True,
-                   ignore_permissions=True)
+    #     doc.customer=self.customer
+    # #     # itemDoc=frappe.new_doc('Sales Order Item')
+    # #     # itemDoc.qty=self.total_rented_hours
+    # #     # itemDoc.delivery_date=today()
+    # #     # itemDoc.item_code=self.stock_item
+    # #     # itemDoc.gst_hsn_code="999512"
+    #     doc.append('items',{
+    #         'item_code':self.stock_item,
+    #         'delivery_date':today(),
+    #         'qty':self.total_rented_hours,
+    #         'rate':self.hourly_rent,
+    #         'amount':self.total_rented_hours*self.hourly_rent,
+    #         'gst_hsn_code':"999512"
+    #     })
+    #     # doc=frappe.get_doc({
+    # #         'doctype':'Sales Order',
+    # #         'customer':self.customer
+    # #     })
+    #     doc.insert(ignore_mandatory=True,
+    #                ignore_permissions=True)
         
-        doc.save()
+    #     doc.save()
    
